079fcb8c124c680b445a6534723bb20454247422
Rehan khan <rehankhanabdali@gmail.com>
2023-03-31 14:23:24 +0500

    working on about section and navbar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,52 @@
 import React from "react";
-
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 export const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me_img">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>6 Months Working</small>
+            </article>
+
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>No</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>2 Completed</small>
+            </article>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+              iste voluptatum mollitia eaque ut sit enim ipsa, minima nihil eum
+              soluta, necessitatibus quis sed, quas vitae reprehenderit
+              accusamus officia repudiandae.
+            </p>
+          </div>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
@@ -6,21 +6,42 @@ import { BiBook } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDetail } from "react-icons/bi";
 export const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#">
+      <a
+        href="#"
+        className={activeNav == "#" ? "active" : ""}
+        onClick={() => setActiveNav("#")}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        className={activeNav == "#about" ? "active" : ""}
+        onClick={() => setActiveNav("#about")}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        className={activeNav == "#experience" ? "active" : ""}
+        onClick={() => setActiveNav("#experience")}
+      >
         <BiBook />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        className={activeNav == "#services" ? "active" : ""}
+        onClick={() => setActiveNav("#services")}
+      >
         <RiServiceLine />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        className={activeNav == "#contact" ? "active" : ""}
+        onClick={() => setActiveNav("#contact")}
+      >
         <BiMessageSquareDetail />
       </a>
     </nav>
